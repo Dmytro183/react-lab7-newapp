6af52bccddfd447c319d6f7393daff231102909d
Air <air@MacBook-Air-Air-2.local>
2025-05-29 08:34:03 +0300

    add main page cards

--- new-app/src/pages/Home.js
+++ new-app/src/pages/Home.js
@@ -1,26 +1,53 @@
 import React from 'react';
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 import CarouselBoxHk from '../components/CarouselBoxHk';
-import { Container, Card, Button } from 'react-bootstrap';
+import team1 from '../assets/team1.jpg';
+import team2 from '../assets/team2.jpg';
+import team3 from '../assets/team3.jpg';
 
 function Home() {
   return (
     <>
+      {/* Карусель */}
       <CarouselBoxHk />
-      <Container>
-        <h2 className="text-center m-4">Our team</h2>
-        <div className="row">
-          <div className="col">
-            <Card className="m-4 text-center" bg="light" border="primary">
-              <Card.Img variant="top" src="https://qwertify.io/wp-content/uploads/2021/04/Team.jpeg" />
+
+      {/* Секція Our team */}
+      <Container className="mt-5 pt-5">
+        <h2 className="text-center mb-4">Our team</h2>
+        <Row>
+          <Col md={4} className="mb-4">
+            <Card className="text-center" bg="light" border="primary">
+              <Card.Img variant="top" src={team1} alt="Team 1" />
               <Card.Body>
                 <Card.Title>Developers</Card.Title>
                 <Card.Text>Команда 1</Card.Text>
-                <Button variant="primary">About team</Button>
+                <Button variant="primary" href="/team1">About team</Button>
               </Card.Body>
             </Card>
-          </div>
-          {/* Додай ще дві команди за аналогією */}
-        </div>
+          </Col>
+
+          <Col md={4} className="mb-4">
+            <Card className="text-center" bg="light" border="success">
+              <Card.Img variant="top" src={team2} alt="Team 2" />
+              <Card.Body>
+                <Card.Title>Designers</Card.Title>
+                <Card.Text>Команда 2</Card.Text>
+                <Button variant="success" href="/team2">About team</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col md={4} className="mb-4">
+            <Card className="text-center" bg="light" border="warning">
+              <Card.Img variant="top" src={team3} alt="Team 3" />
+              <Card.Body>
+                <Card.Title>Marketing</Card.Title>
+                <Card.Text>Команда 3</Card.Text>
+                <Button variant="warning" href="/team3">About team</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </>
   );
